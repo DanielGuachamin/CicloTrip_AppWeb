6386c29247738d732a29474011827a42a46cb8a4
DanielGuachamin <guillermo.guachamin@epn.edu.ec>
2022-09-03 21:28:59 -0500

    Variables colocadadas para produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBLGOVxBDyNc9m75N-8g09KPohWabUBhhk",
+    authDomain: "politrip.firebaseapp.com",
+    databaseURL: "https://politrip-default-rtdb.firebaseio.com",
+    projectId: "politrip",
+    storageBucket: "politrip.appspot.com",
+    messagingSenderId: "39828289178",
+    appId: "1:39828289178:web:7920be6841ead142875123",
+    measurementId: "G-WR0FZG19SG"
+  }
 };
